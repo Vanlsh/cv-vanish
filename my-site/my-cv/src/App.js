17d4dda2b7d349c2b271c27bc6b6079857c043a2
Vanlsh <iparan800@ukr.net>
2022-03-06 20:08:14 +0200

    add site
    add site

---
+++ my-site/my-cv/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+import Home from "./ProtfolioContainer/Home/Home";
+
+function App() {
+  console.log("Hello World!!!");
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
